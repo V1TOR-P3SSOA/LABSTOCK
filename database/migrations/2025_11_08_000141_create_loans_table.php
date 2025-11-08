164dc194a823e6fff1a8ac6796f0543c7e7f9a12
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-07 21:30:22 -0300

    feat(Migrations): criação das colunas da tabela de empréstimos/reservas

--- database/migrations/2025_11_08_000141_create_loans_table.php
+++ database/migrations/2025_11_08_000141_create_loans_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->string( 'equipment_id');
+            $table->string('user_id');
+            $table->date('borrow_date');
+            $table->date('return_date');
+            $table->string('status');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('loans');
