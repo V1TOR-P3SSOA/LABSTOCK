1a2e357b5f15dae3e457e4c243eea6add13969d3
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-07 21:29:40 -0300

    feat(Migrations): criação das colunas da tabela das pesquisas

--- database/migrations/2025_11_08_000103_create_researches_table.php
+++ database/migrations/2025_11_08_000103_create_researches_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('researches', function (Blueprint $table) {
             $table->id();
+            $table->string( 'title');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('user_id');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('researches');
