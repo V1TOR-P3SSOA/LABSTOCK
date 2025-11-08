d5d98b633de773346797742b4b52b4722857b220
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-07 21:28:32 -0300

    feat(Migrations): criação das colunas da tabela de reagentes

--- database/migrations/2025_11_07_235933_create_reagents_table.php
+++ database/migrations/2025_11_07_235933_create_reagents_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('reagents', function (Blueprint $table) {
             $table->id();
+            $table->string( 'name');
+            $table->string('formula');
+            $table->decimal('quantity');
+            $table->string('unit');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('reagents');
