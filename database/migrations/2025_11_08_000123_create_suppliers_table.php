83d8f973a0b6f99449533e7e7ae33c379d3a6ebe
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-07 21:29:53 -0300

    feat(Migrations): criação das colunas da tabela de fornecedores

--- database/migrations/2025_11_08_000123_create_suppliers_table.php
+++ database/migrations/2025_11_08_000123_create_suppliers_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->string( 'name');
+            $table->string('cnpj');
+            $table->string('email');
+            $table->string('address');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('suppliers');
