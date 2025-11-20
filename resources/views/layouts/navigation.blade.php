9aa03d23fc2ee187654fe22ea3af660253147e4c
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-20 19:07:14 -0300

    new(navigation): redireionamentos no link do header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                         {{ __('Reagentes') }}
                     </x-nav-link>
 
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('equipments.index')" :active="request()->routeIs('equipments.*')">
                         {{ __('Equipamentos') }}
                     </x-nav-link>
 
