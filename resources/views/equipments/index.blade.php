b139ead99975a0283b5527cc97e0bea3a9da1180
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-20 19:06:49 -0300

    feat(equipaments): criação da view de listagem dos quipamentos

---
+++ resources/views/equipments/index.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Equipamnetos') }}
+        </h2>
+    </x-slot>
+
+    <a href="{{ route('equipments.create') }}">Adicionar novo equipamento</a>
+
+    <table>
+        <tr>
+            <td>NOME</td>
+            <td>CÓDIGO</td>
+            <td>STATUS</td>
+            <td>ÚLTIMA CALIBRAÇÃO</td>
+        </tr>
+        @foreach ($equipments as $equipment)
+            <tr>
+                <td>{{ $equipment -> name }}</td>
+                <td>{{ $equipment -> asset_code }}<td>
+                <td>{{ $equipment -> status }}</td>
+                <td>{{ $equipment -> last_calibration }}</td>
+                <td>
+                    <a href="{{ route('reagents.edit', $equipment -> id) }}">Editar</a> | <form action="{{ route('reagents.destroy', $equipment -> id) }}" method="POST" id="delete">
+                        @csrf 
+                        @method('DELETE')
+                        <button type="submit">Excluir</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+</x-app-layout>
