ca0d690c52f8a4c8d6d81a15c1a4025cac7aef5c
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-20 17:50:35 -0300

    feat(web):criação das rotas de reagentes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReagentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('reagents', ReagentController::class);
 
 require __DIR__.'/auth.php';
