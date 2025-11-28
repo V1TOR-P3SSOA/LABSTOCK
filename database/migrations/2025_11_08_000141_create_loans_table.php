d900393a30e0bdaac802962f61c1bce7543a8943
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-27 23:23:05 -0300

    fix(Loans): ajuste das colunas equipment_id e user_id para chaves estrangeiras

--- database/migrations/2025_11_08_000141_create_loans_table.php
+++ database/migrations/2025_11_08_000141_create_loans_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
-            $table->string( 'equipment_id');
-            $table->string('user_id');
+            $table->foreignId('equipment_id')->constrained('equipments');
+            $table->foreignId('user_id')->constrained('users');
             $table->date('borrow_date');
-            $table->date('return_date');
+            $table->date('return_date')->nullable();
             $table->string('status');
             $table->timestamps();
         });
