2d033d6caf8407d7dce3f93d78a93feb03407e87
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-27 23:19:55 -0300

    feat(Equipment): adição de trechos para a comunicação entre tabelas

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Loan;
 
 class Equipment extends Model
 {
+    use HasFactory;
+    protected $table = 'equipments';
     protected $fillable = ['name', 'asset_code', 'status', 'last_calibration'];
+
+    public function loans(): HasMany
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
