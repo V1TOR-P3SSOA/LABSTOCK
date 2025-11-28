3279b428d62b8ef062a077d427a69dc6d66d7243
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-28 14:08:09 -0300

    feat(Equipments): corrigir título da página de criação de equipamentos

--- resources/views/equipments/create.blade.php
+++ resources/views/equipments/create.blade.php
@@ -4,32 +4,42 @@
             {{ __('Equipamentos') }}
         </h2>
     </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h1 class="text-2xl font-semibold mb-6">Adiconar novo equipamento</h1>
+                    <form class="form" action="{{ route('equipments.store') }}" method="POST">
+                        @csrf
+                        <div>
+                            <label for="name" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Nome:</label>
+                            <input type="text" id="name" name="name"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                        </div>
+                        <div>
+                            <label for="asset_code" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Código:</label>
+                            <input type="text" id="formula" name="asset_code"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                        </div>
+                        <div>
+                            <label for="status" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Status:</label>
+                            <select id="status" name="status" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                                <option value="">Reservado/Disponível</option>
+                                <option value="Reservado">Reservado</option>
+                                <option value="Disponível">Disponível</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="last_calibration" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Última calibração:</label>
+                            <input type="date" id="unit" name="last_calibration"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                        </div>
+                        <div class="flex items-center gap-4 pt-4">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Cadastrar</button>
+                            <a href="{{ route('equipments.index') }}" class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Voltar</a>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <h1>ADICIONAR NOVO EQUIPAMENTO</h1>
-    <form class="form" action="{{ route('equipments.store') }}" method="POST">
-        @csrf 
-      
-        <label for="name">Nome:</label>
-        <input type="text" id="name" name="name"><br>
-
-        <label for="asset_code">Código:</label>
-        <input type="text" id="asset_code" name="asset_code"><br>
-
-        <label for="status">Status
-        <select name="status" id="status">
-
-            <option value="" disabled selected> </option>
-
-            <option value="Livre">Livre</option>
-
-            <option value="Reservado">Reservado</option>
-
-        </select>
-
-        <label for="last_calibration">Última Calibração:</label>
-        <input type="date" id="last_calibration" name="last_calibration"><br>
-        
-        <button type="submit">Adicionar</button>
-        <a href="{{ route('equipments.index') }}">Cancelar</a>
-</form>
 </x-app-layout>
