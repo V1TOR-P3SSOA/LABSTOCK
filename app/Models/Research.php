5763ca153ef3d09159134d006d118d0335809b20
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-28 14:07:45 -0300

    feat(Research): criação do modelo Research com relacionamentos e atributos preenchíveis

---
+++ app/Models/Research.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\User;
+
+class Research extends Model
+{
+    protected $table = 'researches';
+    protected $fillable = ['title', 'start_date', 'end_date', 'user_id'];
+
+    use HasFactory;
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function researches(): HasMany
+    {
+        return $this->hasMany(Research::class);
+    }   
+}
