c620351eac658f23bc1e5c951bd4f23110f14bf3
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-28 15:07:40 -0300

    feat(EditLoan): adicionar campos para seleção de equipamento, solicitante e datas de retirada e devolução no formulário de edição de reservas

---
+++ resources/views/loans/edit.blade.php
@@ -0,0 +1,54 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Reservas') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h1 class="text-2xl font-semibold mb-6">Editar reserva</h1>
+                    <form class="form" action="{{ route('loans.update', $loan->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div>
+                            <label for="equipment_id" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Equipamento:</label>
+                            <select id="equipment_id" name="equipment_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                                @foreach ($equipments as $equipment)
+                                    <option value="{{ $equipment->id }}">
+                                        {{ $equipment->name }} (Cód: {{ $equipment->asset_code }})
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <label for="user_id" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Solicitante:</label>
+                            <select id="user_id" name="user_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                                @foreach ($users as $user)
+                                    <option value="{{ $user->id }}">
+                                        {{ $user->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            <label for="quantity" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Data de retirada:</label>
+                            <input type="text" id="quantity" name="quantity" value="{{ $loan->borrow_date }}"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                        </div>
+                        <div>
+                            <label for="unit" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Data de devolução:</label>
+                            <input type="text" id="unit" name="unit" value="{{ $loan->return_date }}"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                        </div>
+                        <div class="flex items-center gap-4 pt-4">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Salvar</button>
+                            <a href="{{ route('loans.index') }}" class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Cancelar</a>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
