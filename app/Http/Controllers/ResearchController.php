6817b9f25f81f00067874f28dae3a46540e0daf4
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-28 14:06:59 -0300

    feat(Research): criação do ResearchController com métodos para gerenciar pesquisas

---
+++ app/Http/Controllers/ResearchController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Research;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ResearchController extends Controller
+{
+        public function index()
+    {
+        $researches = Research::with(['user'])->get();
+        return view('researches.index', compact('researches'));
+    }
+
+    public function create()
+    {
+        $users = User::all();
+        return view('researches.create', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['user_id'] = auth()->id();
+        Research::create($data);
+        return redirect()->route('researches.index')->with('success', 'Pesquisa cadastrada com sucesso!');
+    }
+
+    public function edit(Research $research)
+    {
+        return view('researches.edit', compact('research'));
+    }
+
+    public function update(Request $request, Research $research)
+    {
+        $research->update($request->all());
+        return redirect()->route('researches.index')->with('sucess', 'Pesquisa atualizada com sucesso!');
+    }
+
+    public function destroy(Research $research)
+    {
+        $research->delete();
+        return redirect()->route('researches.index')->with('sucess', 'Pesquisa excluída com sucesso!');
+    }
+}
