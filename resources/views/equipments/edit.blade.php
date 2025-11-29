84a18c9524b9e45050a60669a023c70772d2b805
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-29 20:00:02 -0300

    feat(edit):adição dos trechos que mostram os erros

--- resources/views/equipments/edit.blade.php
+++ resources/views/equipments/edit.blade.php
@@ -15,10 +15,16 @@
                         <div>
                             <label for="name" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Nome:</label>
                             <input type="text" id="name" name="name" value="{{ $equipment->name }}"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            @error('name')
+                                <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div>
                             <label for="asset_code" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Código:</label>
                             <input type="text" id="formula" name="asset_code" value="{{ $equipment->asset_code }}"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            @error('asset_code')
+                                <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div>
                             <label for="status" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Status:</label>
@@ -27,10 +33,16 @@
                                 <option value="Reservado">Reservado</option>
                                 <option value="Disponível">Disponível</option>
                             </select>
+                            @error('status')
+                                <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div>
                             <label for="last_calibration" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Última calibração:</label>
                             <input type="date" id="unit" name="last_calibration" value="{{ $equipment->last_calibration }}"  class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            @error('last_calibration')
+                                <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div class="flex items-center gap-4 pt-4">
                             <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Salvar</button>
