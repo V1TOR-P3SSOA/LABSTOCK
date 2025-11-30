63a7e1549c2f06717afa830f41b57ccaed2a1415
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-30 17:06:48 -0300

    feat(Web): validação de usuário logado no sistema

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('reagents', ReagentController::class);
+    Route::resource('equipments', EquipmentController::class);
+    Route::resource('researches', ResearchController::class);
+    Route::resource('loans', LoanController::class);
+    Route::resource('suppliers', SupplierController::class);
 });
-
-Route::resource('reagents', ReagentController::class);
-Route::resource('equipments', EquipmentController::class);
-Route::resource('researches', ResearchController::class);
-Route::resource('loans', LoanController::class);
-Route::resource('suppliers', SupplierController::class);
 
 
 require __DIR__.'/auth.php';
