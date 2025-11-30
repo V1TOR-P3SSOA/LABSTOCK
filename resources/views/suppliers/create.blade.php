77fc18699ff3ffcec76d5322b7987839e74ab91e
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-30 11:51:00 -0300

    feta(create): alteração da estilização para cores do sistema para views de fornecedores

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -20,28 +20,28 @@
                         </div>
                         <div>
                             <label for="cnpj" class="block font-medium text-sm text-gray-700 dark:text-gray-300">CNPJ:</label>
-                            <input type="text" id="cnpj" name="cnpj" value="{{ old('cnpj') }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            <input type="text" id="cnpj" name="cnpj" value="{{ old('cnpj') }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
                             @error('cnpj')
                                 <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
                             @enderror
                         </div>
                         <div>
                             <label for="email" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Email:</label>
-                            <input type="text" id="email" name="email" value="{{ 'email' }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            <input type="text" id="email" name="email" value="{{ old('email') }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-blue-600 rounded-md shadow-sm block mt-1 w-full">
                             @error('email')
                                 <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
                             @enderror
                         </div>
                         <div>
                             <label for="address" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Endereço:</label>
-                            <input type="text" id="address" name="address" value="{{ old('address') }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            <input type="text" id="address" name="address" value="{{ old('address') }}" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
                             @error('address')
                                 <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
                             @enderror
                         </div>
                         <div class="flex items-center gap-4 pt-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Cadastrar</button>
-                            <a href="{{ route('suppliers.index') }}" class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">Voltar</a>
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-blue-600 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-blue-700 dark:hover:bg-white focus:bg-blue-700 dark:focus:bg-white active:bg-blue-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Cadastrar</button>
+                            <a href="{{ route('suppliers.index') }}" class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-gray-800">Voltar</a>
                         </div>
                         
                     </form>
