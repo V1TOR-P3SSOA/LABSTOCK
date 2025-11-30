d4cbdb7d98a4815b062bcd818fffba00e0041cc3
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-29 21:10:55 -0300

    fix(LoanController): correção de conflito do laravel

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Loan;
 use App\Models\Equipment;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUpdateLoan;
 
 class LoanController extends Controller
 {
@@ -21,25 +21,23 @@ class LoanController extends Controller
         $users = User::all();
         return view('loans/create', compact('equipments', 'users'));
     }
-
-    public function store(Request $request)
+    public function store(StoreUpdateLoan $request)
     {
-        Loan::create($request->all());
+        Loan::create($request->validated());
+        
         return redirect()->route('loans.index')->with('sucess', 'reserva registrada com sucesso!');
     }
 
     public function edit(Loan $loan)
-    {
-        $loan = Loan::findOrFail($loan->id); 
+    { 
         $equipments = Equipment::all();
         $users = User::all();
         return view('loans.edit', compact('loan', 'equipments', 'users'));
     }
-
-    public function update(Request $request, Loan $loan)
+    public function update(StoreUpdateLoan $request, Loan $loan)
     {
-        $loan = Loan::findOrFail($loan->id);
-        $loan->update($request->all());
+        $loan->update($request->validated());
+        
         return redirect()->route('loans.index')->with('success', 'Reserva atualizada!');
     }
 
