b762539b74a9acb65a15bae380cc7f29069f1ec3
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-11-30 11:49:05 -0300

    feat(create): alteração da estilização para cores do sistema para views de reservas

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -15,7 +15,7 @@
                         @csrf
                         <div>
                             <label for="user_id" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Usuário (Solicitante):</label>
-                            <select id="user_id" name="user_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                            <select id="user_id" name="user_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
                                 <option value="">Selecione um usuário</option>
                                 @foreach ($users as $user)
                                     <option value="{{ $user->id }}">
@@ -29,7 +29,7 @@
                         </div>
                         <div>
                             <label for="equipment_id" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Equipamento:</label>
-                            <select id="equipment_id" name="equipment_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                            <select id="equipment_id" name="equipment_id" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
                                 <option value="">Selecione um equipamento</option>
 
                                 @foreach ($equipments as $equipment)
@@ -44,21 +44,21 @@
                         </div>
                         <div>
                             <label for="borrow_date" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Data de Retirada:</label>
-                            <input type="date" id="borrow_date" name="borrow_date" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                            <input type="date" id="borrow_date" name="borrow_date" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
                             @error('borrow_date')
                                 <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
                             @enderror
                         </div>
                         <div>
                             <label for="return_date" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Data Prevista de Devolução:</label>
-                            <input type="date" id="return_date" name="return_date" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
+                            <input type="date" id="return_date" name="return_date" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full">
                             @error('return_date')
                                 <p class="mt-1 text-sm text-red-600 dark:text-red-400">{{ $message }}</p>
                             @enderror
                         </div>
                         <div>
                             <label for="status" class="block font-medium text-sm text-gray-700 dark:text-gray-300">Status:</label>
-                            <select id="status" name="status" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
+                            <select id="status" name="status" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-blue-500 dark:focus:border-indigo-600 focus:ring-blue-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full" required>
                                 <option value="Reservado">Reservado</option>
                                 <option value="Emprestado">Emprestado</option>
                                 <option value="Disponível">Disponível</option>
@@ -68,7 +68,7 @@
                             @enderror
                         </div>
                         <div class="flex items-center gap-4 pt-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-blue-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-blue-700 dark:hover:bg-white focus:bg-blue-700 dark:focus:bg-white active:bg-blue-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                 Cadastrar Reserva
                             </button>
                             <a href="{{ route('loans.index') }}" class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
